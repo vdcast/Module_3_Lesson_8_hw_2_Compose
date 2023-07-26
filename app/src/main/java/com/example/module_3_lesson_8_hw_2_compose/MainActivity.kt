4377728c73dce1d8c16b8c6756dc420470d4370b
VD <vdfree448@protonmail.com>
2023-07-26 16:16:10 +0300

    init 2

--- app/src/main/java/com/example/module_3_lesson_8_hw_2_compose/MainActivity.kt
+++ app/src/main/java/com/example/module_3_lesson_8_hw_2_compose/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.module_3_lesson_8_hw_2_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.module_3_lesson_8_hw_2_compose.ui.theme.Module_3_Lesson_8_hw_2_ComposeTheme
@@ -17,12 +20,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Module_3_Lesson_8_hw_2_ComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +32,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello bro!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Module_3_Lesson_8_hw_2_ComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
